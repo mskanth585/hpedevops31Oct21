ae687da6abbb5e6c192190b53e81a1b9d3df4a70
mskanth585 <93465455+mskanth585@users.noreply.github.com>
2021-11-13 11:30:28 +0530

    Modified python.py

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ print("Hello, World!")
 print("Hello, INDIA!")
 print("Hello, Hyderabad!")
 
-
-
 print("Hello, Team!")
-print("Hello, Team!")
+print("Hello, Pavan!")
 
+print("Hello, World!")
+print("Hello, INDIA!")
+print("Hello, Bangalore!")
